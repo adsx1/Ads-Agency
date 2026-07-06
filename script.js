ca1c34bdb7f1fcb746fd7cb9025c6d5548493fe5
ads <simranmehta573@gmail.com>
2026-07-06 17:07:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,58 +13,22 @@ document.addEventListener("DOMContentLoaded", () => {
     /*==================================
       BUTTON CLICK
     ==================================*/
-
-    const joinBtn = document.querySelector(".join-btn");
-
-    joinBtn.addEventListener("click", function (e) {
+const joinBtn = document.querySelector(".join-btn");
+joinBtn.addEventListener("click", function (e) {
 
     e.preventDefault();
 
     fbq('track', 'Lead');
 
-    setTimeout(function () {
+    fbq('trackCustom', 'TelegramClick');
+
+    setTimeout(() => {
         window.location.href = "https://t.me/+PrLuWP-LxPk2NjI1";
-    }, 500);
+    }, 200);
 
 });
 
-
-
-    /*==================================
-      IMAGE LAZY LOAD
-    ==================================*/
-
-    const images = document.querySelectorAll(".proof-card img");
-
-    images.forEach((img) => {
-
-        img.loading = "lazy";
-
-        img.draggable = false;
-
-    });
-
-
-
-
-    /*==================================
-      PREVENT IMAGE DRAG
-    ==================================*/
-
-    document.querySelectorAll("img").forEach((img) => {
-
-        img.addEventListener("dragstart", (e) => {
-
-            e.preventDefault();
-
-        });
-
-    });
-
-
-
-
-    /*==================================
+ /*==================================
       BUTTON RIPPLE
     ==================================*/
 
@@ -117,46 +81,3 @@ document.addEventListener("DOMContentLoaded", () => {
 END
 
 =========================================*/
-const popup = document.getElementById("imagePopup");
-
-const popupImg = document.getElementById("popupImage");
-
-const closeBtn = document.querySelector(".close-popup");
-
-document.querySelectorAll(".proof-card img").forEach(img => {
-
-    img.addEventListener("click", () => {
-
-        popup.classList.add("active");
-
-        popupImg.src = img.src;
-
-    });
-
-});
-
-closeBtn.addEventListener("click", () => {
-
-    popup.classList.remove("active");
-
-});
-
-popup.addEventListener("click", (e) => {
-
-    if (e.target === popup) {
-
-        popup.classList.remove("active");
-
-    }
-
-});
-
-document.addEventListener("keydown", (e) => {
-
-    if (e.key === "Escape") {
-
-        popup.classList.remove("active");
-
-    }
-
-});
